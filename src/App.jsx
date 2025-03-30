aa4b593a1d6ba6523f84dd4902941893c6cb938e
ToprakArslann <toprakarslan0139@gmail.com>
2025-03-30 18:22:54 +0100

    added rotate,edit,move buttons and gismo to viewport

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Canvas, useFrame, useThree } from "@react-three/fiber";
-import { OrbitControls, Html, Edges } from "@react-three/drei";
+import { OrbitControls, Html, Edges, GizmoHelper, GizmoViewport } from "@react-three/drei";
 import { useEffect, useRef, useState } from "react";
 import * as THREE from "three";
 
@@ -51,7 +51,8 @@ const CubeR = ({onDraggingChange}) => {
       e.stopPropagation();
       setSelected(!selected);
     }}>
-      <boxGeometry args={[5,0.5,3]}/>
+      <boxGeometry args={[6,0.5,5]}/>
+      <pointLight position={[0,2,0]} intensity={10} color={0xffffff}/>
       <meshLambertMaterial color="595959" emissive={"#595959"}/>
     
       {selected && (
@@ -62,23 +63,45 @@ const CubeR = ({onDraggingChange}) => {
               <lineBasicMaterial color={"0xff0000"}/>
             </lineSegments>
           </Edges>
-          <Html position={[0,0.3,0]}>
-            <button style={{
-              background: "white",
-              padding: "8px 16px",
+          <Html position={[3,0.3,0]}>
+            <div style={{
+              padding: "8px",
+              gap: "8px",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "flex-start",
+              alignItems: "flex-start",
               borderRadius: "4px",
               border: "none",
-              cursor: "grab",
-              transform: "translateX(-50%)",
-            }}
-            onMouseDown={(e) =>{
-              e.stopPropagation();
-              setIsDragging(true);
-            }}
-            onMouseUp={() => setIsDragging(false)}
-            >
-              {isDragging ? "Moving..." : "Move"}
-            </button>
+            }}>
+              <button style={{
+                background: "white",
+                margin: "auto",
+                cursor: "grab",
+              }}
+              onMouseDown={(e) =>{
+                e.stopPropagation();
+                setIsDragging(true);
+              }}
+              onMouseUp={() => setIsDragging(false)}
+              >
+                <img src="src/assets/hand-pointer-solid.svg" style={{width: "20px"}} draggable="false"/>
+              </button>
+              <button style={{
+                background: "white",
+                margin: "auto",
+                cursor: "grab",
+                }}>
+                <img src="src/assets/rotate-solid.svg" style={{width: "20px"}} draggable="false"/>
+              </button>
+              <button style={{
+                background: "white",
+                margin: "auto",
+                cursor: "grab",
+                }}>
+                <img src="src/assets/marker-solid.svg" style={{width: "20px"}} draggable="false"/>
+              </button>
+            </div>
           </Html>
         </>
       )}
@@ -99,6 +122,10 @@ const App = () => {
       <spotLight position={[0,5,0]} intensity={10} color={0xffffff}/>
       <color attach="background" args={["#000000"]}/>
       <gridHelper args={[1000,500,'red', 'white']}/>
+      <GizmoHelper
+      alignment="bottom-right" margin={[80,80]} >
+        <GizmoViewport axisColors={["red", "green", "blue"]} labelColor="black"/>
+      </GizmoHelper>
       <CubeR onDraggingChange={handleDraggingChange}/>
     </Canvas>
   )
